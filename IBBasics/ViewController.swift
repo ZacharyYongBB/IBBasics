8e4ccd335f8b15127499919bb953e0f88abc0533
ZacharyYongBB <zacharyyong_1997@outlook.com>
2022-09-03 03:10:06 +0800

    added button to change background colour

--- IBBasics/ViewController.swift
+++ IBBasics/ViewController.swift
@@ -8,12 +8,27 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var lightOn = true
 
+    @IBOutlet var myButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        myButton.tintColor = .red
+        
         // Do any additional setup after loading the view.
     }
 
+    fileprivate func extractedFunc() {
+        view.backgroundColor = lightOn ? .white :.black
+    }
+    
+    @IBAction func buttonPressed(_ sender: Any) {
+        print("button pressed")
+        lightOn.toggle()
+        extractedFunc()
+    }
 
 }
 
