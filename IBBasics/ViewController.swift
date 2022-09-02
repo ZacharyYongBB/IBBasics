fe8abebf4b4a3bf9e269ebf682fbb9fbd877fe0d
ZacharyYongBB <zacharyyong_1997@outlook.com>
2022-09-03 03:57:11 +0800

    changed interactive button & made whole screen touchable

--- IBBasics/ViewController.swift
+++ IBBasics/ViewController.swift
@@ -9,25 +9,34 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet var LightButton: UIButton!
     var lightOn = true
 
     @IBOutlet var myButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
+        updateUI()
         
         myButton.tintColor = .red
         
         // Do any additional setup after loading the view.
     }
 
-    fileprivate func extractedFunc() {
-        view.backgroundColor = lightOn ? .white :.black
+    fileprivate func updateUI() {
+        if lightOn {
+            view.backgroundColor = .white
+            LightButton.setTitle("Off", for: .normal)
+        }
+        else {
+            view.backgroundColor = .black
+            LightButton.setTitle("On", for: .normal)
+        }
     }
     
     @IBAction func buttonPressed(_ sender: Any) {
         print("button pressed")
         lightOn.toggle()
-        extractedFunc()
+        updateUI()
     }
 
 }
